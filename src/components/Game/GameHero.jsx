85f5c57bbe977acea50cfbac925ce9f52a37b523
Daniel Cueto <dcueto14.info@gmail.com>
2023-11-24 11:03:19 +0100

    global-styles

--- src/components/Game/GameHero.jsx
+++ src/components/Game/GameHero.jsx
@@ -4,49 +4,33 @@ import { useEffect, useState } from "react";
 import GameImg from './GameImg';
 
 
-        
-c
-
 
 
 function GameHero({data}){
-    
-    
-    return (  
-        <section className="gameHero">                            
 
-         
+    return (
+        <section className="gameHero">
+
         <h1 className="NameId">{data.id}</h1>
-         
 
         <img className="imgBackground" src ={data.background_image}/>
-        
+
         <p className="NameGame" >{data.name}</p>
-        
-       
+
         <div className="PlatformsAny">
-            
             <p className="Platformsone">Platforms: {data.platform}</p> 
-
-            
         </div>
 
-       
-        
         <div className="Releaseday">
-             <p>Realeased: {data.released}</p>
+            <p>Realeased: {data.released}</p>
         </div>
-              
+
 
         <div className="NameMetacritic" >
              <span className="NumberMeta">{data.metacritic}</span>
              <p className="nameMeta">Metacritic</p>
-                
              </div>
-             
-             
              <div className="containerone">
-                 
         </div>
     
         
